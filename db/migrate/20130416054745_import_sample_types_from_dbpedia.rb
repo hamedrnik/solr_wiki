bc9860679c53730711565a7a40dfbb1d6345a3b2
Hamed Ramezanian <iceage2098@gmail.com>
2013-04-16 11:06:10 +0430

    changes importing migration

--- db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
+++ db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
@@ -1,12 +1,13 @@
 class ImportSampleTypesFromDbpedia < ActiveRecord::Migration
   def up
-    Page.limit(10).each_with_index do |p, i|
+    Page.all.each_with_index do |p, i|
       types = Dbpedia.search(p.title).first.classes.collect(&:label)
       if !types.empty?
+        p.types = []
         types.each do |type|
-          p.types << Type.find_or_create_by(name: type)
+          p.types << Type.find_or_create_by_name!(name: type)
         end
-        p.title.gsub!(/[\_]/, ' ')
+        p.title = p.title.gsub(/([\_])/, ' ')
         p.save!
         puts "record #{i} is saved with title of #{p.title}"
       end
