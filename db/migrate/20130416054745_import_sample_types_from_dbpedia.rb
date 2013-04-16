2882f5cb6c200a120e098c37db4ae878b4fe4bcd
Hamed Ramezanian <iceage2098@gmail.com>
2013-04-16 12:31:13 +0430

    updates migration

--- db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
+++ db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
@@ -1,16 +1,26 @@
 class ImportSampleTypesFromDbpedia < ActiveRecord::Migration
   def up
-    Page.all.each_with_index do |p, i|
-      types = Dbpedia.search(p.title).first.classes.collect(&:label)
-      if !types.empty?
-        p.types = []
-        types.each do |type|
-          p.types << Type.find_or_create_by_name!(name: type)
+    @offset = 0
+    @limit = 100
+    @pages_count = Page.count
+
+    while @offset <= @pages_count do
+      @pages = Page.order('popularity desc').offset(@offset).limit(@limit)
+
+      @pages.each_with_index do |p, i|
+        types = Dbpedia.search(p.title).first.classes.collect(&:label)
+        if !types.empty?
+          p.types = []
+          types.each do |type|
+            p.types << Type.find_or_create_by_name!(name: type)
+          end
+          p.title = p.title.gsub(/([\_])/, ' ')
+          p.save!
+          puts "record #{@offset + 1 + i} is saved with title of #{p.title}"
         end
-        p.title = p.title.gsub(/([\_])/, ' ')
-        p.save!
-        puts "record #{i} is saved with title of #{p.title}"
       end
+
+      @offset += @limit
     end
   end
 
