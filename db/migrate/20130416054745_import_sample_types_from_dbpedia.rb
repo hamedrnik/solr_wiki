35f147b3d5578d16b104028aaa3c8038b7e08cf5
Hamed Ramezanian <iceage2098@gmail.com>
2013-04-16 14:48:45 +0430

    adds pure sql for migration

--- db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
+++ db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
@@ -2,28 +2,40 @@ class ImportSampleTypesFromDbpedia < ActiveRecord::Migration
   def up
     @offset = 0
     @limit = 100
-    @pages_count = Page.count
+    @pages_count = (execute 'select count(*) from pages;').first["count(*)"]
 
     while @offset <= @pages_count do
-      @pages = Page.order('popularity desc').offset(@offset).limit(@limit)
+      @pages = execute "select id, title from pages order by popularity desc limit #{@limit} offset #{@offset}"
 
       @pages.each_with_index do |p, i|
-        results = Dbpedia.search(p.title)
+        results = Dbpedia.search(p["title"])
         unless results.empty?
           types = results.first.classes.collect(&:label)
           if !types.empty?
-            p.types = []
+            # deletes old types of page
+            execute "delete from pages_types where page_id = #{p['id']}"
+
             types.each do |type|
-              p.types << Type.find_or_create_by_name!(name: type)
+              @types = execute "select id from types where name like '#{type}'"
+              if @types.length > 0
+                execute "insert into pages_types (type_id, page_id, created_at, updated_at) values (#{@types.first['id']}, #{p['id']}, '#{Time.zone.now}', '#{Time.zone.now}')"
+              else
+                execute "insert into types (name, created_at, updated_at) values ('#{type}', '#{Time.zone.now}', '#{Time.zone.now}')"
+                new_type = Type.find_by_name(type)
+                execute "insert into pages_types (type_id, page_id, created_at, updated_at) values (#{new_type.id}, #{p['id']}, '#{Time.zone.now}', '#{Time.zone.now}')"
+              end
             end
-            p.title = p.title.gsub(/([\_])/, ' ')
-            p.save!
-            puts "record #{@offset + 1 + i} is saved with title of #{p.title}"
+
+            # updates title
+            new_title = p['title'].gsub(/([\_])/, ' ')
+            execute "update pages set title = '#{new_title}' where id = #{p['id']}"
+
+            puts "record #{@offset + 1 + i} is saved with title of #{p['title']}"
           end
         else
-          p.title = p.title.gsub(/([\_])/, ' ')
-          p.save!
-          puts "#{p.title} is not found in Dbpedia"
+          new_title = p['title'].gsub(/([\_])/, ' ')
+          execute "update pages set title = '#{new_title}' where id = #{p['id']}"
+          puts "#{p['title']} is not found in Dbpedia"
         end
       end
 
