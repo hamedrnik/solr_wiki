c6eba3733304452af1d4ce273657f491e19005ff
Hamed Ramezanian <iceage2098@gmail.com>
2013-04-17 00:45:47 +0430

    optimizes migration

--- db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
+++ db/migrate/20130416054745_import_sample_types_from_dbpedia.rb
@@ -1,15 +1,15 @@
 class ImportSampleTypesFromDbpedia < ActiveRecord::Migration
   def up
     @offset = 0
-    @limit = 100
+    @limit = 1000
     @pages_count = (execute 'select count(*) from pages;').first.first.to_i
 
     while @offset <= @pages_count do
-      @pages = (execute "select id, title from pages order by popularity desc limit #{@limit} offset #{@offset}")
+      @pages = (execute "select id, title from pages order by id asc limit #{@limit} offset #{@offset}")
 
       @pages.each_with_index do |p, i|
         page_id = p.first
-        page_title = p[1].gsub(/_/, ' ')
+#        page_title = p[1].gsub(/_/, ' ')
         page_title = page_title.gsub(/\\/, '\&\&').gsub(/'/, "''")
 
         results = Dbpedia.search(p[1])
@@ -33,11 +33,11 @@ class ImportSampleTypesFromDbpedia < ActiveRecord::Migration
             end
 
             # updates title
-            execute "update pages set title = '#{page_title}' where id = #{page_id}"
+#            execute "update pages set title = '#{page_title}' where id = #{page_id}"
             puts "record #{@offset + 1 + i} is saved with title of #{page_title}"
           end
         else
-          execute "update pages set title = '#{page_title}' where id = #{page_id}"
+#          execute "update pages set title = '#{page_title}' where id = #{page_id}"
           puts "#{page_title} is not found in Dbpedia"
         end
       end
