98ba9a62f88d30494ef89b488185fdbada0b2a82
Hamed Ramezanian <iceage2098@gmail.com>
2013-04-22 13:19:05 +0430

    updates migration

--- db/migrate/20130421063910_load_types_from_dbpedia_db.rb
+++ db/migrate/20130421063910_load_types_from_dbpedia_db.rb
@@ -6,22 +6,24 @@ class LoadTypesFromDbpediaDb < ActiveRecord::Migration
     counter = 1
     RDF::Reader.open(File.join(Rails.root, "db", "instance_types_en.nt")) do |reader|
       reader.each_statement do |statement|
-        article = statement.subject.to_s.scan(/http:\/\/.*\/(.*)/).first.first
-        type = statement.object.to_s.scan(/http:\/\/.*\/(.*)/).first.first
-        article.gsub!(/_/, ' ') if article
+        if statement && statement.subject && statement.object
+          article = statement.subject.to_s.scan(/http:\/\/.*\/(.*)/).first.first
+          type = statement.object.to_s.scan(/http:\/\/.*\/(.*)/).first.first
+          article.gsub!(/_/, ' ')
 
-        pages = Page.search do
-          fulltext article
-          paginate :page => 1, :per_page => 1
-        end.results
+          pages = Page.search do
+            fulltext article
+            paginate :page => 1, :per_page => 1
+          end.results
 
-        unless pages.empty?
-          page = pages.first
-          if type && !type.empty?
-            page.types << Type.find_or_create_by_name(type)
-            puts "record #{counter} is saved with title of #{page.title} and type of #{type}"
-            puts "=========================================================================="
-            counter += 1
+          unless pages.empty?
+            page = pages.first
+            if type && !type.empty?
+              page.types << Type.find_or_create_by_name(type)
+              puts "record #{counter} is saved with title of #{page.title} and type of #{type}"
+              puts "=========================================================================="
+              counter += 1
+            end
           end
         end
       end
