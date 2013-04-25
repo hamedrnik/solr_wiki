17c76cfa80e20b016d2dfd883fc3c664d57b4d0b
Hamed Ramezanian <iceage2098@gmail.com>
2013-04-25 21:08:57 +0430

    fixes a mistake

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,10 +1,4 @@
 class TypesController < ApplicationController
   def index
-#    @types = Type.search do
-#      fulltext params[:type_search]
-#      order_by :created_at, :desc
-#    end.results
-
-#    render json: @types
-#  end
+  end
 end
